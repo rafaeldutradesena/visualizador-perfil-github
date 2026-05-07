640469ad2634081e4f66b1d94fb556728934f239
Rafael Dutra <rafaeldutrasena@gmail.com>
2026-05-06 23:20:29 -0300

    feat: adiciona total de seguidores e seguindo e código refatorado

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,24 @@
+import { fetchGithubUser } from './githubApi.js';
+import { renderProfile } from './profileView.js';
+
 const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 const profileResults = document.querySelector('.profile-results');
 
-const BASE_URL = 'https://api.github.com';
-
 btnSearch.addEventListener('click', async () => {
-  const userName = inputSearch.value;
-
-  if (userName) {
+    const userName = inputSearch.value;
+    if (!userName) {
+        alert('Por favor, digite um nome de usuário do GitHub.');
+        profileResults.innerHTML = "";
+        return;
+    }
     profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
-    
     try {
-        const response = await fetch(`${BASE_URL}/users/${userName}`);
-        
-        if (!response.ok) {
-            alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
-            profileResults.innerHTML = "";
-            return;
-        }
-
-        const userData = await response.json();
-
-        profileResults.innerHTML = `
-        <div class="profile-card">
-            <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
-            <div class="profile-info">
-                <h2>${userData.name}</h2>
-                <p>${userData.bio || 'Não possui bio cadastrada 😢.'}</p>
-            </div>
-        </div>`;
-
+        const userData = await fetchGithubUser(userName);
+        renderProfile(userData, profileResults);
     } catch (error) {
         console.error('Erro ao buscar o perfil do usuário:', error);
-        alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.');
+        alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
         profileResults.innerHTML = "";
     }
-
-  } else {
-    alert('Por favor, digite um nome de usuário do GitHub.');
-    profileResults.innerHTML = "";
-  }
 });
